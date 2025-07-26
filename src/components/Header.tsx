e834e64d7fe9e90ace1c7d56b6125dcaf7b6c8b1
ronak-pal1 <ronakpaul882@gmail.com>
2025-07-27 00:06:25 +0530

    Very basic page for pre-release

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,18 @@
-import { Link } from "react-router-dom";
 import Icon from "../assets/icon.png";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center justify-between  py-5 px-28 bg-transparent backdrop-blur-3xl border-b border-b-slate-200 fixed top-0 left-0 z-50">
+    <header className="w-full flex flex-row items-center justify-between py-3 px-4 sm:px-8 md:px-16 lg:px-28 bg-transparent backdrop-blur-3xl border-b border-b-slate-200 fixed top-0 left-0 z-50 gap-3 sm:gap-0">
       {/* Left section */}
-
-      <div className="flex items-center space-x-15">
-        <div className="flex items-center space-x-3">
-          <img src={Icon} alt="hashdome icon" className="w-7" />
-          <h1 className="text-2xl font-medium text-blue-600">Hashdome</h1>
-        </div>
-
-        <div className="flex items-center space-x-10">
-          <Link to={""}>About</Link>
-          <Link to={""}>Solutions</Link>
-          <Link to={""}>Blogs</Link>
-          <Link to={""}>Solutions</Link>
-          <Link to={""}>Pricing</Link>
-        </div>
+      <div className="flex items-center space-x-3 mb-2 sm:mb-0">
+        <img src={Icon} alt="hashdome icon" className="w-6 sm:w-7" />
+        <h1 className="text-xl sm:text-2xl hidden sm:block font-medium text-blue-600">
+          Hashdome
+        </h1>
       </div>
-
       {/* Right section */}
       <div className="flex items-center">
-        <button className="bg-blue-600 text-neutral-200 text-sm rounded-full px-8 py-2 cursor-pointer">
+        <button className="bg-blue-600 text-neutral-200 text-[10px] sm:text-sm rounded-full px-6 sm:px-8 py-2 cursor-pointer w-full sm:w-auto">
           Book a quick call
         </button>
       </div>
--- src/pages/Home/components/HeroSection.tsx
+++ src/pages/Home/components/HeroSection.tsx
@@ -1,27 +1,14 @@
 const HeroSection = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center relative">
-      <div className="w-[60%] flex flex-col items-center justify-center text-center">
-        <div className="w-fit bg-yellow-200 px-7 py-2 text-sm rounded-full font-semibold font-comfortaa">
+    <div className="w-full h-screen flex items-center justify-center relative pt-24 sm:pt-0">
+      <div className="w-[95%] sm:w-[80%] md:w-[70%] flex flex-col items-center justify-center text-center">
+        <div className="w-fit bg-neutral-50 border border-neutral-200 px-5 sm:px-7 py-2 mb-6 sm:mb-9 text-[10px] sm:text-sm rounded-full font-comfortaa">
           <p>Best software services in the planet</p>
         </div>
-
-        <h1 className="text-7xl leading-28">
+        <h1 className="text-4xl sm:text-6xl md:text-8xl leading-tight font-extralight">
           Land on a Lengendary{" "}
-          <span className="text-blue-600 font-medium font-comfortaa">
-            Brand Planet
-          </span>
+          <span className="text-blue-600 font-comfortaa">Brand Planet</span>
         </h1>
-
-        <button className="mt-3 border-2 border-blue-600 text-blue-600 rounded-full px-20 py-2 cursor-pointer">
-          Explore
-        </button>
-      </div>
-
-      <div className="absolute bottom-0 left-0 w-full ">
-        <svg height="80" width="1440" xmlns="http://www.w3.org/2000/svg">
-          <ellipse rx="720" ry="150" cx="720" cy="160" fill="#155dfc" />
-        </svg>
       </div>
     </div>
   );
